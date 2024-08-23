f6b9c7c9bcd1bbee517e1eedc29a51c929aa4485
David <dumana92@gmail.com>
2024-08-22 20:45:34 -0400

    First Push

---
+++ Develop/server/src/routes/api/historyRoutes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+const router = Router();
+
+import HistoryService from '../../service/historyService.js';
+import historyService from '../../service/historyService.js';
+
+router.get('/', async (_req, res) => {
+  try {
+    const savedStates = await HistoryService.getStates();
+    res.json(savedStates);
+    console.log('States successfully retrieved from search history');
+    console.log('===============================================');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ msg: 'State id is required' });
+    }
+    await historyService.removeState(req.params.id);
+    res.json({ success: 'State successfully removed from search history' });
+    console.log('State successfully removed from search history');
+    console.log('===============================================');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+export default router;
---
+++ Develop/server/src/routes/api/parkRoutes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+const router = Router();
+
+import HistoryService from '../../service/historyService.js';
+import ParkService from '../../service/parkService.js';
+
+router.get('/:state', async (req, res) => {
+  try {
+    const stateName = req.params.state;
+    const stateCode = await ParkService.convertStateNameToCode(stateName);
+    const parks = await ParkService.getParksByState(stateCode);
+    //ensures saved data has proper casing regardless of input
+    const sanitizedStateName = await ParkService.convertStateCodeToName(
+      stateCode
+    );
+    await HistoryService.addState(sanitizedStateName);
+    res.json(parks);
+    console.log('Parks successfully retrieved');
+    console.log('===============================================');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/events/:state', async (req, res) => {
+  try {
+    const state = req.params.state;
+    const stateCode = await ParkService.convertStateNameToCode(state);
+    const events = await ParkService.getClosestEventByState(stateCode);
+    if (typeof events === 'string') {
+      res.status(404).json({ message: 'No events found' });
+    } else {
+      res.json(events);
+      console.log('Events successfully retrieved');
+      console.log('===============================================');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+export default router;
